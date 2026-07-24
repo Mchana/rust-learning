5e434c9ec8aa1883c8b7d3977199799701fbbfba
Muntej Chana <Muntejchana@hotmail.com>
2026-07-24 15:46:10 +0000

    got to interacting with move

--- understanding-ownership/src/main.rs
+++ understanding-ownership/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     code_to_break_things_up();
+    ownership_demonstration();
     return_multiple_values();
+    string_example();
+    string_example_mutation();
+    memory_example();
+    interacting_with_move();
 }
 
 //Ownership
@@ -32,8 +37,95 @@ fn code_to_break_things_up() -> i32{
     //serves no purpose, just to break up the notes lol
 }
 
+//Ownership Rules
+//-->Each value in Rust has an owner.
+//-->There can only be one owner at a time.
+//-->When the owner goes out of scope, the value will be dropped.
+
+fn ownership_demonstration()
+    {                      // s is not valid here, since it's not yet declared
+        let _s = "hello";   // s is valid from this point forward
+
+        // do stuff with s
+    }                      // this scope is now over, and s is no longer valid
+
+//In other words, there are two important points in time here:
+
+//-->When s comes into scope, it is valid.
+//-->It remains valid until it goes out of scope.
 
 
+//String Type
+//To demonstrate the rules of ownership, we need a more complex data type 
+//the types previously discussed are of known size, and can be popped on and off the stack when their scope is over
+//the String type is stored on the heap so is best to demonstrate Ownership
+
+//We've used string literals before, but they aren't suitable for every situation involving text
+//they're immutable , and not every string value can be known when we write our code, e.g. user input
+//this type manages data allocated on the heap
+
+fn string_example(){
+    let _s =String::from("hello");
+    //:: allows us to namespace this from without using string_from
+    //ngl no idea what that means yet 
+}
+
+fn string_example_mutation(){
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{s}"); // this will print `hello, world!`
+
+} //why can string be mutated but literals cannot? it's because of how these types deal with memory
+
+//--Memory and allocation
+
+//with string literals, we know the contents at compile time, so the text is hardcoded directly into the final exe
+//this doesn't work with unknown text that might change size
+
+//With the String type, in order to support a mutable, growable piece of text, 
+//we need to allocate an amount of memory on the heap, unknown at compile time, to hold the contents. This means:
+
+//--The memory must be requested from the memory allocator at runtime.
+//--We need a way of returning this memory to the allocator when we’re done with our String.
+
+//when we call String::from the implementation requests the memory it needs
+// Unlike other languages with no garbage collectors, Rust automatically returns memory when it goes out of scope
+
+fn memory_example(){
+        let _s = String::from("hello"); // s is valid from this point forward
+
+        // do stuff with s
+    }                                  // this scope is now over, and s is no longer valid
+
+//at this point, Rust calls a special function for us called 'drop' to return the memory
+//it does this on  a }
+
+//Variables and Data Interacting with Move:
+
+//Multiple variables can interact with the same data in different ways in Rust. 
+//for example, an integet and a string
+
+fn interacting_with_move(){
+    let x = 5;
+    let _y = x;
+
+    let s1 = String::from("hello");
+    let _s2 = s1;
+
+//we would assume these work in different ways, however they are different under the hood
+}
+
+//a String is made of 3 parts - a pointer, length and capacity
+//the pointer points to where the String is stored on the heap in memory
+//the length is how much memory, in bytes the contents of String is using
+//the capacity is how much memory in bytes it has been allocated
+// for example "hello" would point to the memory, with a length of 5 and a capacity of 5
+// in memory it would have an index (1,2,3,4,5) representing (h,e,l,l,o) in the heap
+// when we assign s1 to s2, the String is copied, meaning we copy the pointer, length and capacity
+//the pointer still points to the same place in the heap, it does not copy the data
+//this saves runtime if the data is large
 
 
 
