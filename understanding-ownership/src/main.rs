92ee004073a5c1bc4346e799f9d0a38ff926073e
Muntej Chana <Muntejchana@hotmail.com>
2026-07-24 10:19:35 +0000

    alias test

--- understanding-ownership/src/main.rs
+++ understanding-ownership/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    return_multiple_values();
 }
+
+//returning multiple values using a tuple
+fn return_multiple_values(){
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("the length of '{s2}' is {len}.");
+}
+
+fn calculate_length(s:String) -> (String, usize) {
+    let length = s.len(); //.len returns length of a string
+    (s, length)
+}
+
+//the above is kind of a pain and there's a better way to do it
+
+//References and Borrowing
