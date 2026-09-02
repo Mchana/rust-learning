76cb6a42dadde52288f0575dd7e1907441592407
Muntej Chana <Muntejchana@hotmail.com>
2026-09-02 08:53:07 +0000

    starting methods idk why the other stuff didn't push?

--- structs-methods/src/main.rs
+++ structs-methods/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
+    calc_rectangle();
 
 }
 
 //Methods are similar to functions: 
+
 //We declare them with the fn keyword and a name, 
 //they can have parameters and a return value, 
 //and they contain some code that’s run when the method is called from somewhere else. 
@@ -12,3 +14,27 @@ fn main() {
 //which represents the instance of the struct the method is being called on.
 
 // --Methods Syntax
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn calc_rectangle() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        " the area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
